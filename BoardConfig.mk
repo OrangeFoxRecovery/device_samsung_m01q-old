c8a3e9f8bd155f339a6dc613b2a810fa255061f0
Sushrut Gupta <sushrut.gupta@yahoo.com>
2021-06-17 00:27:25 +0530

    SoC: Upgrade to arm64

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,11 +22,17 @@ DEVICE_PATH := device/samsung/m01q
 ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := m01q
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,7 +21,7 @@
 FDEVICE="m01q"
 
 # m01q specific
-export TARGET_ARCH=arm
+export TARGET_ARCH=arm64
 export TARGET_DEVICE_ALT="m01q,m01qxx,m01qlte,m01qltexx"
 export OF_TARGET_DEVICES="m01q,m01qxx,m01qlte,m01qltexx"
 export FOX_RECOVERY_INSTALL_PARTITION="dev/block/platform/soc/7824900.sdhci/by-name/recovery"
